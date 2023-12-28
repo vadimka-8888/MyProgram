1a33aba13bc39ff2b8a6080be6f883d6ba60c6a5
vadimka-8888 <vadim-ivanov-20010510@mail.ru>
2023-12-28 22:18:04 +0300

    Program is now here!:)

---
+++ SimpleProgram/SimpleProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Type probability of random variable: ");
+string? input = Console.ReadLine();
+float p;
+if (!float.TryParse(input, out p))
+{
+    p = 0.0f;
+}
+float res = 0;
+Console.WriteLine("Type numbers and then '#' to compute result: ");
+input = Console.ReadLine();
+int num;
+while (input != "#")
+{
+    if (!int.TryParse(input, out num))
+    {
+        num = 0;
+    }
+    res += num * p;
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"The mathematical expectation is: {res}");
+
